75b192522eb3f584f71fa9dea96319f2a86c39dd
FlaBBB <f12345ff67@gmail.com>
2023-10-27 22:08:40 +0700

    fix: SECRET var get from env

--- backend/util/hashing.go
+++ backend/util/hashing.go
@@ -3,9 +3,17 @@ package util
 import (
 	"crypto/sha256"
 	"fmt"
+	"os"
 )
 
-const SECRET = "hXwGc-Niw.hq,fu:v+HK8|_uVA;xunkGB$u5]NS^%?G7MmFF'm7qD)M=J#S.2G&"
+var SECRET string
+
+func init() {
+	SECRET = os.Getenv("HASH_SECRET")
+	if SECRET == "" {
+		SECRET = "hXwGc-Niw.hq,fu:v+HK8|_uVA;xunkGB$u5]NS^%?G7MmFF'm7qD)M=J#S.2G&"
+	}
+}
 
 func HashPassword(password string) string {
 	h := sha256.New()
